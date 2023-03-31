dc2d77e1b4075cfcccb6f80ed6c1afb9c685fb62
Takaaki Aoki <takaaki.aoki.work@gmail.com>
2023-03-31 15:49:46 +0900

    cimplify the return value of hotential function

--- R/HodgePotentialHumanFlow-package.R
+++ R/HodgePotentialHumanFlow-package.R
@@ -46,11 +46,25 @@ scalar_potential_with_details = function(od_table){
 #' @param flow_on_edges A data.frame with three columns: vertex1, vertex2, and netflow.
 #' - num_samples: Monte calro samples (default = 1e5)
 #' - seed: Monte calro samples (default = -1, then generate it by std::random_device)
+#' @return This function returns a data.frame with three columns: zone, potential, p-value
+#' @export
+scalar_potential_on_graph = function(flow_on_edges, num_samples = 1e5, seed =-1){
+  unique_geozones = unique(union(flow_on_edges$vertex1, flow_on_edges$vertex2))
+  weight = rep(1, length(flow_on_edges$vertex1) )
+  res = pontential_on_graph(flow_on_edges$vertex1, flow_on_edges$vertex2, weight, flow_on_edges$netflow,unique_geozones, num_samples, seed)
+  return(res$value) 
+}
+
+#' This function calculates scalar potential of human flow for a given origin-destionation matrix (detailed version)
+#'
+#' @param flow_on_edges A data.frame with three columns: vertex1, vertex2, and netflow.
+#' - num_samples: Monte calro samples (default = 1e5)
+#' - seed: Monte calro samples (default = -1, then generate it by std::random_device)
 #' @return This function returns a list:
 #' - value: a data.frame with three columns: zone, potential, p-value
 #' - R2: percentage of gradient component
 #' @export
-scalar_potential_on_graph = function(flow_on_edges, num_samples = 1e5, seed =-1){
+scalar_potential_on_graph_details = function(flow_on_edges, num_samples = 1e5, seed =-1){
   unique_geozones = unique(union(flow_on_edges$vertex1, flow_on_edges$vertex2))
   weight = rep(1, length(flow_on_edges$vertex1) )
   res = pontential_on_graph(flow_on_edges$vertex1, flow_on_edges$vertex2, weight, flow_on_edges$netflow,unique_geozones, num_samples, seed)
